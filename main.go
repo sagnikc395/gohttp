f217da9e5a98b01c715db390f2c6f8bfa660f675
Sagnik Chatterjee <sagnikchatterjee607@gmail.com>
2026-01-07 18:48:53 -0500

    feat: start working on get lines channel

--- main.go
+++ main.go
@@ -11,8 +11,37 @@ import (
 func getLinesChannel(f io.ReadCloser) <-chan string {
 	out := make(chan string, 1)
 
-	go func() {}()
+	go func() {
+		defer f.Close()
+		defer close(out)
 
+		for {
+			str := ""
+
+			for {
+				data := make([]byte, 8)
+				n, err := f.Read(data)
+				if err != nil {
+					break
+				}
+
+				data = data[:n]
+
+				if i := bytes.IndexByte(data, '\n'); i != -1 {
+					str += string(data[:i])
+					data = data[i+1:]
+					out <- str
+					str = ""
+				}
+
+				str += string(data)
+			}
+
+			if len(str) != 0 {
+				out <- str
+			}
+		}
+	}()
 	return out
 }
 
@@ -21,28 +50,10 @@ func main() {
 	if err != nil {
 		log.Fatal("error", "error", err)
 	}
-	str := ""
+	lines := getLinesChannel(f)
 
-	for {
-		data := make([]byte, 8)
-		n, err := f.Read(data)
-		if err != nil {
-			break
-		}
-
-		data = data[:n]
-
-		if i := bytes.IndexByte(data, '\n'); i != -1 {
-			str += string(data[:i])
-			data = data[i+1:]
-			fmt.Printf("read: %s\n", str)
-			str = ""
-		}
-
-		str += string(data)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
 	}
 
-	if len(str) != 0 {
-		fmt.Printf("read: %s\n", str)
-	}
 }
