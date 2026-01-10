fa2b1d4c841e24d28d731b4da1bc3e245d7a5f8c
Sagnik Chatterjee <sagnikchatterjee607@gmail.com>
2026-01-10 16:24:16 -0500

    feat: added proper request from header

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,6 +1,8 @@
 package request
 
 import (
+	"errors"
+	"fmt"
 	"io"
 	"strings"
 )
@@ -15,11 +17,6 @@ type Request struct {
 	RequestLine RequestLine
 	// Headers     map[string]string
 	// Body        []byte
-}
-
-// validation check for request line header
-func (r *RequestLine) ValidateHTTP() bool {
-	return r.HttpVersion == "HTTP/1.1"
 }
 
 func parseRequestLine(b string) (*RequestLine, string, error) {
@@ -37,18 +34,44 @@ func parseRequestLine(b string) (*RequestLine, string, error) {
 		return nil, restOfMsg, ERROR_MALFORMED_REQUEST_LINE
 	}
 
+	parts = strings.Split(parts[2], "/")
+
+	if len(parts) != 3 {
+		return nil, restOfMsg, ERROR_MALFORMED_REQUEST_LINE
+	}
+
+	//HTTP validation , should be 1.1 only
+	httpParts := strings.Split(parts[2], "/")
+	if len(httpParts) != 2 || httpParts[0] != "HTTP" || httpParts[1] != "1.1" {
+		return nil, restOfMsg, ERROR_MALFORMED_REQUEST_LINE
+	}
+
 	rl := &RequestLine{
 		Method:        parts[0],
 		RequestTarget: parts[1],
-		HttpVersion:   parts[2],
+		HttpVersion:   httpParts[1],
 	}
-	if rl.ValidateHTTP() {
-		return rl, restOfMsg, nil
-	}
-	return nil, restOfMsg, ERROR_UNSUPORTED_HTTP_VERSION
+
+	return rl, restOfMsg, nil
 
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, errors.Join(
+			fmt.Errorf("unable to io.ReadAll"),
+			err,
+		)
+	}
 
+	str := string(data)
+
+	rl, _, err := parseRequestLine(str)
+	if err != nil {
+		return nil, err
+	}
+	return &Request{
+		RequestLine: *rl,
+	}, err
 }
